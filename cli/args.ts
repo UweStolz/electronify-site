a96780e3de125717a91e2829c31be3dd87bd497d
UweStolz <uwe.stolz@protonmail.com>
2019-10-12 14:57:40 +0200

    fix: optional arguments

--- cli/args.ts
+++ cli/args.ts
@@ -2,19 +2,20 @@
 
 import * as yargs from 'yargs';
 
-// eslint-disable-next-line no-unused-expressions
-yargs
-  .command('build', 'Build an archive for your desired website.')
-  .option('url', {
-    alias: 'u',
-    type: 'string',
-    description: 'Your URL you want to build an archive for.',
-  })
-  .check((argument) => {
-    let url;
-    if (argument.url) {
-      url = new URL(argument.url);
-    }
-    return url;
-  })
-  .argv;
+export default async function initialize(args: string[]): Promise<any> {
+  // eslint-disable-next-line no-unused-expressions
+  yargs
+    .option('url', {
+      alias: 'u',
+      type: 'string',
+      description: 'Your URL you want to build an archive for.',
+    })
+    .check((argument) => {
+      if (argument.url) {
+        const url = new URL(argument.url);
+        return url;
+      }
+      return true;
+    })
+    .parse(args);
+}
