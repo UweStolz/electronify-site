5c967c2eb66df136024591dfc87945cf85cafa47
UweStolz <uwe.stolz@protonmail.com>
2019-10-12 14:57:53 +0200

    feat: basic call structure

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-export default async function execute(): Promise<void> {
+import logger from './cli/logger';
+import * as ask from './cli/ask';
+import initialize from './cli/args';
 
+function logGreeting(): void {
+  logger.info('🔥🔥🔥🔥🔥🔥🔥🔥🔥');
+  logger.info('Hello there..');
+  logger.info('🔥🔥🔥🔥🔥🔥🔥🔥🔥');
+}
+
+function logFarewell(): void {
+  logger.info('Have fun!');
+}
+
+export default async function execute(): Promise<void> {
+  try {
+    await initialize(process.argv);
+    await logGreeting();
+    await ask.default();
+    logFarewell();
+  } catch (error) {
+    logger.error(error);
+  }
 }
