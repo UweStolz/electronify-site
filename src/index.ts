02128718b67962b72522f168834979db111d53a4
UweStolz <uwe.stolz@protonmail.com>
2019-10-12 11:53:28 +0200

    ref: path

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ function createWindow(): void {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(app.getAppPath(), 'preload.js'),
       nodeIntegration: true,
     },
     titleBarStyle: process.platform === 'darwin' ? 'hidden' : undefined,
