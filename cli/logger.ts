45f46a81c6f70a5f9684ef26b7cc3d72b1a2c174
UweStolz <uwe.stolz@protonmail.com>
2019-10-12 14:06:25 +0200

    build: add pretty print

--- cli/logger.ts
+++ cli/logger.ts
@@ -1,5 +1,14 @@
 import pino from 'pino';
 
-const logger: pino.Logger = pino();
+const logger = pino({
+  prettyPrint: {
+    colorize: true,
+    crlf: false,
+    errorLikeObjectKeys: ['err', 'error'],
+    errorProps: '',
+    translateTime: 'HH:MM:ss',
+  },
+  base: null,
+});
 
 export default logger;
