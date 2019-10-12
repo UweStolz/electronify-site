89e7a810aabcdeabfc67c09a83f4aea89d235c59
UweStolz <uwe.stolz@protonmail.com>
2019-10-12 20:26:15 +0200

    build: ignore strings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": true, "peerDependencies": true}],
-    "@typescript-eslint/ban-ts-ignore": "off"
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': true, 'optionalDependencies': true, 'peerDependencies': true}],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'max-len': [0, { code: 100, ignoreStrings: true }],
   },
 
   parser: "@typescript-eslint/parser",
