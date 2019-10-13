94ab79d0c19ce2552889b504f05236b1fcba3d33
UweStolz <uwe.stolz@protonmail.com>
2019-10-13 16:44:19 +0200

    feat: add data pre-processig

--- builder/index.ts
+++ builder/index.ts
@@ -1,20 +1,45 @@
 
 import builder from 'electron-builder';
 import { Choices } from '../util/values';
+import logger from '../cli/logger';
 
-const { Platform } = builder;
+function getBaseName(url: string): string {
+  const host = new URL(url).hostname;
+  const baseName = host.substring(0, 4) === 'www.'
+    ? host.substring(4, host.lastIndexOf('.'))
+    : host.substring(0, host.lastIndexOf('.'));
+  return baseName;
+}
+
+async function preprocessData(choices: Choices): Promise<Choices> {
+  const OS: string = choices.os.toUpperCase();
+  const name: string = getBaseName(choices.url);
+  const data: Choices = {
+    url: name,
+    os: OS,
+    format: choices.format,
+    architecture: choices.architecture,
+  };
+  return data;
+}
 
 export default async function buildArtifact(choices: Choices): Promise<void> {
+  const { Platform } = builder;
+  const data = await preprocessData(choices);
   builder.build({
-    targets: Platform.LINUX.createTarget(),
+    // @ts-ignore
+    targets: Platform[data.OS].createTarget(),
     config: {
-      productName: 'someName',
+      productName: data.url,
+      appId: `com.electron.${data.url}`,
+      artifactName: `electronify-${data.url}.${data.format}`,
     },
   })
     .then(() => {
     // handle result
     })
     .catch((error) => {
-    // handle error
+      logger.error('An error occurred while creating the artifact!');
+      logger.error(error);
     });
 }
