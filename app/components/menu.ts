f40e115f6f2f4249eb632890d7fad61bbe7b17f4
UweStolz <uwe.stolz@protonmail.com>
2019-10-19 19:42:00 +0200

    feat: add basic menu

---
+++ app/components/menu.ts
@@ -0,0 +1,62 @@
+import {
+  Menu, MenuItemConstructorOptions, MenuItem,
+} from 'electron';
+
+function buildMenuTemplate(): (MenuItemConstructorOptions | MenuItem)[] {
+  const template: (MenuItemConstructorOptions | MenuItem)[] = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectAll',
+        },
+      ],
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize',
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close',
+        },
+      ],
+    },
+  ];
+  return template;
+}
+
+
+export default function buildeMenu(): void {
+  const template = buildMenuTemplate();
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
