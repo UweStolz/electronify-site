10f797ad1a787ddb4476536f4dcbb20477144691
UweStolz <uwe.stolz@protonmail.com>
2020-06-18 12:52:41 +0200

    ref: resolve index

--- bin/electronify.js
+++ bin/electronify.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-
-const electronify = require('../index');
+const { join } = require('path');
+const path = join(__dirname, '..');
+const index = `${path}/index`;
+const electronify = require(index);
 
 electronify.default();
